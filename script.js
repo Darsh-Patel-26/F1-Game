665ce2538a6ae0a746a78566fa5f4aecf078f477
Darsh-Patel-26 <darsh.git.26@gmail.com>
2024-08-01 14:58:45 +0530

    Record Button bug fix

--- script.js
+++ script.js
@@ -1,46 +1,65 @@
-let bulbs = document.querySelectorAll('div.bulb')
-let i = 0
-let offtime,
-  flag = false
-let display = document.querySelector('#time-diff')
+let bulbs = document.querySelectorAll('div.bulb');
+let i = 0;
+let offtime, flag = false, id, abort = false;
+let display = document.querySelector('#time-diff');
 
-function switchL (i) {
-  let color = bulbs[i].style.backgroundColor
-  bulbs[i].style.backgroundColor = color === 'red' ? '#591212' : 'red'
-  bulbs[i + 1].style.backgroundColor = color === 'red' ? '#591212' : 'red'
+function switchL(i) {
+  let color = bulbs[i].style.backgroundColor;
+  bulbs[i].style.backgroundColor = color === 'red' ? '#591212' : 'red';
+  bulbs[i + 1].style.backgroundColor = color === 'red' ? '#591212' : 'red';
 }
 
-function start () {
-  display.value = ''
-  flag = false
+function start() {
+  display.value = '';
+  flag = false;
+  
+  if (abort) {
+    clearInterval(id);
+    abort = false;
+    i = 0;
+  }
+
   if (i < bulbs.length && i + 1 < bulbs.length) {
-    switchL(i)
+    switchL(i);
   }
-  i += 2
+  
+  i += 2;
+
   if (i >= bulbs.length) {
-    setTimeout(() => {
-      const randomDelay = Math.floor(Math.random() * (3000 - 200 + 1)) + 200
-      setTimeout(reset, randomDelay)
-    }, 1000)
-    i = 0
-    return
+    id = setTimeout(() => {
+      const randomDelay = Math.floor(Math.random() * (3000 - 200 + 1)) + 200;
+      setTimeout(reset, randomDelay);
+    }, 1000);
+    i = 0;
+    return;
   }
-  setTimeout(start, 1000)
+  
+  setTimeout(start, 1000);
 }
 
-function reset () {
+function reset(bool = true) {
+  clearTimeout(id);
+  
   for (let d of bulbs) {
-    d.style.backgroundColor = '#591212'
+    d.style.backgroundColor = '#591212';
   }
-  offtime = new Date()
-  flag = true
+  
+  if (bool) {
+    offtime = new Date();
+    flag = true;
+  }
 }
 
-function record () {
+function record() {
   if (flag) {
-    const recTime = new Date()
-    const diff = (recTime - offtime) / 1000
-    const res = diff.toFixed(3)
-    if (res != NaN) display.value = res
+    const recTime = new Date();
+    const diff = (recTime - offtime) / 1000;
+    const res = diff.toFixed(3);
+    
+    if (!isNaN(res)) display.value = res;
+    flag = false;
+  } else {
+    reset(false);
+    abort = true;
   }
 }
